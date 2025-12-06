28a16d7c28b3259b2db6946abda2c20f96c0c285
SayanjitDas-web <thewebman01@gmail.com>
2025-12-06 14:00:42 +0530

    document socket updated

--- src/sockets/document.socket.js
+++ src/sockets/document.socket.js
@@ -1,2 +1,26 @@
-import Document from "../models/Document.model.js"
+import Document from "../models/Document.model.js";
 
+export default function documentSocket(io, socket) {
+  socket.on("connectDocument", async (sessionId, callback) => {
+    const document = await Document.findOne({ sessionId });
+    if (!document) {
+      callback({ success: false, message: "session expired" });
+    }
+
+    if (document.sessionId !== sessionId) {
+      callback({ success: false, message: "session expired" });
+    }
+    socket.join(sessionId);
+  });
+
+  socket.on("sendDocument", async ({ sessionId, docId }) => {
+    const document = await Document.findById(docId);
+    if (!document) {
+      io.to(sessionId).emit(
+        "document",
+        "document with the given id not found!"
+      );
+    }
+    io.to(sessionId).emit("document", document);
+  });
+}
