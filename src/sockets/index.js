2ca3ad4abebcd54e145cb32b571b1b329db55f04
SayanjitDas-web <thewebman01@gmail.com>
2025-12-06 14:02:07 +0530

    socket index updated

--- src/sockets/index.js
+++ src/sockets/index.js
@@ -1,8 +1,11 @@
+import documentSocket from "./document.socket.js"
 
 export default function registerSocketHandlers(io) {
 
   io.on("connection", (socket) => {
     console.log(`Socket connected: ${socket.id}`);
+
+    documentSocket(io, socket)
 
     socket.on("disconnect", () => {
       console.log("Socket Disconnected:", socket.id);
