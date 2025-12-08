069e8687a4ef4e8f357985a6337fa18ef86bf841
SayanjitDas-web <thewebman01@gmail.com>
2025-12-08 16:12:49 +0530

    document router updated

--- src/routes/document.route.js
+++ src/routes/document.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createDocument, deleteDocument, updateSessionId, getDocument } from "../controllers/document.controller.js"
+import { createDocument, deleteDocument, updateSessionId, getDocument, getAllDocument } from "../controllers/document.controller.js"
 import { auth } from "../middlewares/auth.js"
 
 const documentRouter = Router()
 
 documentRouter.get("/:docId", auth, getDocument)
+documentRouter.get("/all", auth, getAllDocument)
 documentRouter.post("/create", auth, createDocument)
 documentRouter.post("/delete", auth, deleteDocument)
 documentRouter.post("/updateSession", auth, updateSessionId)
