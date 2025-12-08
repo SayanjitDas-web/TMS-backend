f70e7e6e15f7be87b463a95bf240247d98a0ce9c
SayanjitDas-web <thewebman01@gmail.com>
2025-12-08 12:09:32 +0530

    all updated

--- src/server.js
+++ src/server.js
@@ -12,31 +12,31 @@ import cookieParser from "cookie-parser";
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(cookieParser())
-app.use(cors({origin:"*", credentials: true}))
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(cookieParser());
+app.use(cors({ origin: `${process.env.CORS_ORIGIN}`, credentials: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 import homeRoute from "./routes/home.route.js";
-app.use("/api/index", homeRoute)
+app.use("/api/index", homeRoute);
 
-import authRouter from "./routes/auth.route.js"
-app.use("/api/auth", authRouter)
+import authRouter from "./routes/auth.route.js";
+app.use("/api/auth", authRouter);
 
-import documentRouter from "./routes/document.route.js"
-app.use("/api/document", documentRouter)
+import documentRouter from "./routes/document.route.js";
+app.use("/api/document", documentRouter);
 
-import blockRouter from "./routes/block.route.js"
-app.use("/api/block", blockRouter)
+import blockRouter from "./routes/block.route.js";
+app.use("/api/block", blockRouter);
 
-import aiRouter from './routes/ai.route.js'
-app.use("/api/ai", aiRouter)
+import aiRouter from "./routes/ai.route.js";
+app.use("/api/ai", aiRouter);
 
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",
-    credentials: true
+    origin: `${process.env.CORS_ORIGIN}`,
+    credentials: true,
   },
 });
 
@@ -46,7 +46,7 @@ connectDb();
 server.listen(port, () => {
   if (process.env.NODE_ENV === "production") {
     console.log(`server is running on port:${port}`);
-  }else{
+  } else {
     console.log(`server is running on http://localhost:${port}`);
   }
 });
